d07a4edead154f70a5df1d75f4219299145dedf7
Chris Racklyeft <chris@theracklyefts.co.uk>
2022-11-22 17:17:52 +0000

    feat: Updated attachment functionality

--- lib/src/couchbase_attachment_dao.dart
+++ lib/src/couchbase_attachment_dao.dart
@@ -4,9 +4,10 @@ import 'dart:typed_data';
 import 'package:cbl/cbl.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_model/flutter_model.dart';
+import 'package:loggy/loggy.dart';
 
 ///
-class CouchbaseAttachmentDAO extends AttachmentDAO {
+class CouchbaseAttachmentDAO extends AttachmentDAO with UiLoggy {
   final Database database;
   CouchbaseAttachmentDAO(this.database);
 
@@ -18,36 +19,51 @@ class CouchbaseAttachmentDAO extends AttachmentDAO {
   @override
   Future<void> savePathPost(
       String fieldName, String srcPath, dynamic id, String? mimeType) async {
+    
+        loggy.debug("savePathPost started fieldName: $fieldName id= $id  srcPath= $srcPath");
+
     var src = File(srcPath);
 
     Document? doc = await database.document(id);
+     loggy.debug("savePathPost Have document ${doc!.toJson()}");
+
     MutableDocument mdco = doc!.toMutable();
     Blob blob = Blob.fromData(mimeType ?? "unknown", await src.readAsBytes());
     mdco.setBlob(blob, key: "${fieldName}blob");
     mdco.setValue({"hasvalue": true, "mimeType": mimeType}, key: fieldName);
+    loggy.debug("savePathPost Saving the document");
     database.saveDocument(mdco);
   }
 
   @override
   Future<Map<String, dynamic>?> savePath(
       String fieldName, String srcPath, String? mimeType) async {
+        loggy.debug("savePath started $fieldName returning empty" );
     return {};
   }
 
   @override
-  Future<ImageProvider> getImageProvider(IModel coverImage,
-      Map<String, dynamic>? details, String field) async {
+  Future<ImageProvider> getImageProvider(
+      IModel coverImage, Map<String, dynamic>? details, String field) async {
     Document? doc = await database.document(coverImage.id);
     Blob? blob = doc!.blob("${field}blob");
-    var content = await blob!.content();
 
-    var f = MemoryImage(content);
-    return f;
+    if (blob != null) {
+      var content = await blob!.content();
+
+      var f = MemoryImage(content);
+      return f;
+    }
+    return MemoryImage(Uint8List(0));
   }
 
   @override
   Future<Map<String, dynamic>?> removeContentPost(
       String fieldName, dynamic id) async {
+
+        loggy.debug("removeContentPost started $fieldName" );
+
+
     //Delete it and return null;
     Document? doc = await database.document(id);
     MutableDocument mdco = doc!.toMutable();
@@ -62,20 +78,24 @@ class CouchbaseAttachmentDAO extends AttachmentDAO {
   @override
   Future<Map<String, dynamic>?> saveContentPost(String fieldName,
       Uint8List data, String? ext, dynamic id, String? mimeType) async {
+    loggy.debug("saveContentPost started update for field $fieldName");
+
     Document? doc = await database.document(id);
     MutableDocument mdco = doc!.toMutable();
     Blob blob = Blob.fromData(mimeType ?? "unknown", data);
     mdco.setBlob(blob, key: "${fieldName}blob");
     mdco.setValue({"hasvalue": true, "mimeType": mimeType}, key: fieldName);
 
+    loggy.debug("saveContentPost saved document with id $id");
     database.saveDocument(mdco);
-    
+
     return null;
   }
 
   @override
   Future<Map<String, dynamic>?> saveContent(
       String fieldName, Uint8List data, String? ext, String? mimeType) async {
+        loggy.debug("saveContent started $fieldName" );
     return {};
   }
 }
